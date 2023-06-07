17b37beb6af95e3338773567a2f780cd2248210b
unknown <facupaccioretti@gmail.com>
2023-06-07 18:16:07 -0300

    cambio en el path de la llamada curl

--- GUAYINI/urls.py
+++ GUAYINI/urls.py
@@ -79,6 +79,6 @@ urlpatterns = [
     path('enviar_mensaje_curl/', views.enviar_mensaje_curl_messagebird, name='enviar_mensaje_curl_messagebird'),
     path('generate_token/', views.generate_token, name='generate_token'),
     path('recibir_webhook/', views.recibir_webhook, name='recibir_webhook'),
-    path('enviar_llamada_curl_messagebird/', views.enviar_llamada_curl_messagebird, name='enviar_llamada_curl_messagebird')
+    path('enviar_llamada_curl/', views.enviar_llamada_curl_messagebird, name='enviar_llamada_curl_messagebird')
     
 ]
