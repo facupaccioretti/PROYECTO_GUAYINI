2d4d06d1c1834f33ad5b4f3dee116a23785677ec
unknown <facupaccioretti@gmail.com>
2023-06-14 22:35:57 -0300

    cambio dns de db

--- GUAYINI/settings.py
+++ GUAYINI/settings.py
@@ -82,13 +82,25 @@ WSGI_APPLICATION = 'GUAYINI.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'guayinidb',
+        'USER': 'postgres',
+        'PASSWORD': 'yM8Q4xrlp5obNDPvnyL5',
+        'HOST': 'guayinidb.guayini.com',
+        'PORT': '5432',
+    }
+}
+
 
 
 # Password validation
